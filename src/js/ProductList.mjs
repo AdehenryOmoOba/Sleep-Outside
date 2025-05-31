73698a5f7063ed08160b6eb812b4ca96ae90d8ed
nilberm <nilber16@outlook.com>
2025-05-31 14:54:22 -0300

    "feat: add product sort feature with UI styling and logic for name and price"

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -22,9 +22,33 @@ export default class ProductList {
 
   async init() {
     const list = await this.dataSource.getData(this.category);
+    this.products = list;
     this.renderList(list);
     document.querySelector(".title").textContent = this.category;
     return list.length;
+  }
+
+  sortAndRender(criteria) {
+    let sortedList = [...this.products];
+
+    switch (criteria) {
+      case "name-asc":
+        sortedList.sort((a, b) => a.Name.localeCompare(b.Name));
+        break;
+      case "name-desc":
+        sortedList.sort((a, b) => b.Name.localeCompare(a.Name));
+        break;
+      case "price-asc":
+        sortedList.sort((a, b) => a.FinalPrice - b.FinalPrice);
+        break;
+      case "price-desc":
+        sortedList.sort((a, b) => b.FinalPrice - a.FinalPrice);
+        break;
+      default:
+        sortedList = [...this.products];
+    }
+
+    this.renderList(sortedList);
   }
 
   renderList(list) {
